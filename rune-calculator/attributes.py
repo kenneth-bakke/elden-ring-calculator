c29a6c004a7bb94ee54dc88ecab68194de7732fa
Kenneth Bakke <kennethsbakke@gmail.com>
2022-04-01 15:19:23 -0700

    Factored out attributes and character details to their own classes

--- rune-calculator/attributes.py
+++ rune-calculator/attributes.py
@@ -1,6 +1,54 @@
-class Attributes:
-    def __init__(self, attributes: dict):
-        self.attributes = attributes
+from cgitb import text
+from tkinter import *
+from tkinter import ttk
 
-    def __getList__(self):
-        return self.attributes.keys()
+
+class Attribute:
+    def __init__(self, root, row_place, name="Placeholder", stat_count=0):
+        self.mainframe = ttk.Frame(root, border=1, borderwidth=1, relief="sunken")
+
+        self.stat_count = stat_count
+
+        self.name = ttk.Label(self.mainframe, text=name, border=1, borderwidth=1, relief="sunken")
+        self.attr_entry = ttk.Entry(self.mainframe, textvariable=self.stat_count)
+        self.attribute_display = ttk.Label(
+            self.mainframe, text=self.stat_count, border=1, borderwidth=1, relief="sunken"
+        )
+        self.inc = ttk.Button(self.mainframe, text="▲", command=self.increment_stat)
+        self.dec = ttk.Button(self.mainframe, text="▼", command=self.decrement_stat)
+
+        self.attr_entry.bind("<Return>", self.set_stat_count)
+
+        self.mainframe.grid(column=0, row=row_place, sticky=(N, W, E, S))
+        self.name.grid(column=1, row=row_place, rowspan=3, sticky=W)
+        self.attr_entry.grid(column=2, row=row_place, rowspan=3, sticky=W)
+        self.attribute_display.grid(
+            column=3,
+            row=row_place,
+            rowspan=3,
+            sticky=W,
+        )
+        self.inc.grid(column=4, row=1, ipady=5, ipadx=5, sticky=E)
+        self.dec.grid(column=4, row=3, ipady=5, ipadx=5, sticky=E)
+
+    def increment_stat(self):
+        self.stat_count = self.stat_count + 1
+        if self.stat_count > 99:
+            self.stat_count = 99
+        self.attribute_display["text"] = self.stat_count
+
+    def decrement_stat(self):
+        self.stat_count = self.stat_count - 1
+        if self.stat_count < 0:
+            self.stat_count = 0
+        self.attribute_display["text"] = self.stat_count
+
+    def get_stat_count(self):
+        return self.stat_count
+
+    def set_stat_count(self, event):
+        print(event)
+        stat = int(self.attr_entry.get())
+        self.stat_count = int(stat)
+        self.attribute_display["text"] = self.stat_count
+        self.attr_entry.delete(0, END)
---
+++ rune-calculator/calculator.py
@@ -0,0 +1,39 @@
+import os
+from telnetlib import EL
+from tkinter import *
+from tkinter import ttk
+from config import ELDEN_RING
+from attributes import Attribute
+
+
+class RuneCalculator:
+    def __init__(self, root):
+
+        root.title("Rune Calculator")
+
+        self.mainframe = ttk.Frame(root, padding="3 12")
+
+        # MAIN FRAMES GRID
+        self.character_details = ttk.Frame(self.mainframe, borderwidth=2, relief="sunken")
+        self.attributes = ttk.Frame(self.mainframe, borderwidth=2, relief="sunken")
+        self.weapons = ttk.Frame(self.mainframe, borderwidth=2, relief="sunken")
+
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.character_details.grid(column=0, row=0, columnspan=4, rowspan=1, sticky=(N, W, E, S))
+        self.attributes.grid(column=0, row=1, sticky=(N, W, E, S))
+        self.weapons.grid(column=1, row=1, sticky=(N, W, E, S))
+
+        # ATTRIBUTES SECTION
+        for i, attribute in enumerate(ELDEN_RING["ATTRIBUTES"]):
+            Attribute(self.attributes, i, attribute, 0)
+
+        # WEAPONS SECTION
+        self.weapons_label = ttk.Label(self.weapons, text="Weapons").grid(column=3, row=2, columnspan=4, sticky=W)
+        self.weapon_categories_var = StringVar(self.weapons, name="Weapon Categories")
+        self.weapon_categories_list = ELDEN_RING["WEAPON_CATEGORIES"].keys()
+        self.weapon_categories_combobox = ttk.Combobox(
+            self.weapons, height=10, justify="left", text="Weapon Categories", textvariable=self.weapon_categories_var
+        )
+        self.weapon_categories_combobox["values"] = [category for category in self.weapon_categories_list]
+        self.weapon_categories_combobox.current(0)
+        self.weapon_categories_combobox.grid(column=3, row=3, sticky=W)
